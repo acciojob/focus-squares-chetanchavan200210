e014f0a6ccc748c0469b7f39c401af0dad813d86
chetanchavan200210 <chetanchavan200210@gmail.com>
2025-09-22 11:59:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const square = document.querySelectorAll('.square');
+const lavender = "#E6E6FA";
+const coffee = "#6F4E37";
+
+square.forEach((box)=>{
+box.add.EventListener('mouseover',
+()=>{
+	box.forEach((otherBox)=>,()=>{
+		if(otherBox !==box){
+otherBox.style.backgroundColor=cofee;
+		}
+	});
+});
+box.addEventListener('mouseout',()=>{
+	square.forEach((s)=>{
+		s.style.backgroundColor=lavender;
+	});
+});
+});
