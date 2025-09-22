cf775c613215b110b51d91709fa7413a88cbf02b
chetanchavan200210 <chetanchavan200210@gmail.com>
2025-09-22 12:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-//your JS code here. If required.
-const square = document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.square');
 const lavender = "#E6E6FA";
 const coffee = "#6F4E37";
 
-square.forEach((box)=>{
-box.add.EventListener('mouseover',
-()=>{
-	box.forEach((otherBox)=>,()=>{
-		if(otherBox !==box){
-otherBox.style.backgroundColor=cofee;
-		}
-	});
-});
-box.addEventListener('mouseout',()=>{
-	square.forEach((s)=>{
-		s.style.backgroundColor=lavender;
-	});
-});
+squares.forEach((box) => {
+  box.addEventListener('mouseover', () => {
+    squares.forEach((otherBox) => {
+      if (otherBox !== box) {
+        otherBox.style.backgroundColor = coffee;
+      }
+    });
+  });
+
+  box.addEventListener('mouseout', () => {
+    squares.forEach((s) => {
+      s.style.backgroundColor = lavender;
+    });
+  });
 });
